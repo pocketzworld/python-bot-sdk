173aedbc70e556936a9663c150b5ba1d93880ee4
Vinicius Cabral <106489329+itzvini@users.noreply.github.com>
2024-04-26 22:16:54 +0000

    tip added

--- src/highrise/__init__.py
+++ src/highrise/__init__.py
@@ -254,6 +254,52 @@ class Highrise:
         except:
             return 
 
+    async def tip(self, user: User, amount: int):
+        #checks if the bot has the amount
+            bot_wallet = await self.get_wallet()
+            bot_amount = bot_wallet.content[0].amount
+            if bot_amount <= amount:
+                await self.chat("Not enough funds")
+                return
+            #converts the amount to a string of bars and calculates the fee
+            """Possible values are: "gold_bar_1",
+            "gold_bar_5", "gold_bar_10", "gold_bar_50", 
+            "gold_bar_100", "gold_bar_500", 
+            "gold_bar_1k", "gold_bar_5000", "gold_bar_10k" """
+            bars_dictionary = {10000: "gold_bar_10k", 
+                               5000: "gold_bar_5000",
+                               1000: "gold_bar_1k",
+                               500: "gold_bar_500",
+                               100: "gold_bar_100",
+                               50: "gold_bar_50",
+                               10: "gold_bar_10",
+                               5: "gold_bar_5",
+                               1: "gold_bar_1"}
+            fees_dictionary = {10000: 1000,
+                               5000: 500,
+                               1000: 100,
+                               500: 50,
+                               100: 10,
+                               50: 5,
+                               10: 1,
+                               5: 1,
+                               1: 1}
+            #loop to check the highest bar that can be used and the amount of it needed
+            tip = []
+            total = 0
+            for bar in bars_dictionary:
+                if amount >= bar:
+                    bar_amount = amount // bar
+                    amount = amount % bar
+                    for i in range(bar_amount):
+                        tip.append(bars_dictionary[bar])
+                        total = bar+fees_dictionary[bar]
+            if total > bot_amount:
+                await self.chat("Not enough funds")
+                return
+            for bar in tip:
+                await self.tip_user(user.id, bar)
+
     async def get_room_users(self) -> GetRoomUsersRequest.GetRoomUsersResponse | Error:
         req_id = str(next(self._req_id))
         self._req_id_registry[req_id] = (q := Queue[Any](maxsize=1))
